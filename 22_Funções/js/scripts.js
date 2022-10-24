ddfcfbfb6076689966d6600f7e86a929348ff867
PedroHenriqueSilva <pedrohsilva.ccomp@gmail.com>
2022-10-24 10:06:10 -0300

    “Function”

---
+++ 22_Funções/js/scripts.js
@@ -0,0 +1,26 @@
+function primeiraFuncao(){
+
+    console.log('Funções');
+}
+
+primeiraFuncao();
+
+function dizerNome(nome){
+
+    console.log("o nome é: " + nome);
+}
+
+dizerNome("Matheus");
+dizerNome("Pedro");
+dizerNome("Xavier");
+
+var nomeBancoDeDados = "João";
+
+dizerNome(nomeBancoDeDados);
+
+function soma(a, b){
+    var soma = a+b;
+    return soma;
+}
+
+console.log(soma(16,8));
