e1f82b2a8b24b3bbd4b7fd93dfbf5b4244ddb363
PedroHenriqueSilva <pedrohsilva.ccomp@gmail.com>
2022-10-24 09:18:22 -0300

    “Class”

---
+++ 20_For/js/scripts.js
@@ -0,0 +1,13 @@
+for(var i = 0; i<10; i++){
+    console.log("rep " + i);
+}
+
+var arr =[10,30,04,05];
+
+for(var j=0; j< arr.length;j++){
+    console.log("pos " + arr[j]);
+}
+
+for(var x = 5; x<135; x*=3){
+    console.log(x);
+}
